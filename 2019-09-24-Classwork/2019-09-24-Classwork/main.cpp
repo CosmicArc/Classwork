76062d50271374197d9ae63c63c8eff1e55a93b6
CosmicArc <55443303+CosmicArc@users.noreply.github.com>
2019-09-24 08:43:02 -0400

    Add files via upload

---
+++ 2019-09-24-Classwork/2019-09-24-Classwork/main.cpp
@@ -0,0 +1,26 @@
+//Name: Nathaniel Sanchez
+//Intro to CS
+//Mr.williams
+//Period 1
+//2019-09-24
+
+#include <iostream>
+
+int main()
+{
+	int a = 100;
+	int b = 200;
+	int c = 300;
+	int d = 400;
+	int e = 500;
+
+
+
+
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << c << std::endl;
+	std::cout << d << std::endl;
+	std::cout << e << std::endl;
+
+}
