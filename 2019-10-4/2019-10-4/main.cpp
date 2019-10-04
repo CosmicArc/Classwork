684b7af9f53d081356efb1546612bbf1902b7dcd
CosmicArc <55443303+CosmicArc@users.noreply.github.com>
2019-10-04 08:30:52 -0400

    Add files via upload

---
+++ 2019-10-4/2019-10-4/main.cpp
@@ -0,0 +1,18 @@
+//Name: Nathaniel Sanchez
+//Intro to CS
+//Mr. Williams
+//Period 1
+//2019-10-4-Classwork
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// 3
+	float pi = 4.0 * (1.0 - (1 / 3.0) + (1 / 5.0) - (1 / 7.0) + (1 / 9.0) - (1 / 11.0));
+	float pi2 = 4.0 * (1.0 - (1 / 3.0) + (1 / 5.0) - (1 / 7.0) + (1 / 9.0) - (1 / 11.0) + (1 / 13.0));
+	cout << "Pi = " << pi << endl;
+	cout << "Pi2 = " << pi2 << endl;
+		
+}
